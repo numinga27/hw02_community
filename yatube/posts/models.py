079ee9b118220ba2d2cbaa0241049860eb8081a5
Андрей Крылов <numinga27@yandex.ru>
2022-05-31 21:42:02 +0400

    ra

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model() 
+User = get_user_model()
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -16,8 +17,9 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+    )
 # Create your models here.
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
