73fcb6a1b9e9faea1256f8cc6d7650f57c3289a3
Андрей Крылов <numinga27@yandex.ru>
2022-06-10 21:51:18 +0400

    1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,25 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Group
+from .constats import MAX_POSTS
 
 
 def index(request):
-
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.order_by('-pub_date')
+    paginator = Paginator(post_list, MAX_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
 
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:MAX_POSTS]
     context = {
         'group': group,
         'posts': posts,
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
